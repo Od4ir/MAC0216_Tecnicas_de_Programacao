564d1f4c95b1ee59bfff8868cd08a96af69d477c
Od4ir <101140991+Od4ir@users.noreply.github.com>
2022-09-14 18:02:04 +0000

    EP1, EP4, EP5 e Estudos de Assembly

---
+++ EP5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define MAX 999999936
+
+int eh_primo(int n);
+void modo_0(int n);
+void modo_1(int n);
+
+
+int main() {
+    int modo, n, i, primo = 0;
+    scanf("%d", &modo);
+    scanf("%d", &n);
+
+    if(!modo) {
+        for(i = n + 1; (i < MAX)&&(!primo); i++) {
+                primo = eh_primo(i);
+                if(primo) printf("%d\n",i);
+            }
+        }
+    }
+    else {
+        int x, y;
+        int p = 0;
+
+        for(x = 2; (n/x >= 2)&&(!p); x++) {
+            if((eh_primo(x))&&(n % x == 0)){
+                y = n/x;
+                if(eh_primo(y)) {
+                    printf("%d %d\n", x, y);
+                    p = 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int eh_primo(int n) {
+    int div, p = 1;
+
+    for(div = 2; (div*div <= n)&&(p); div++) {
+        if(n % div == 0) {
+            p = 0;
+        }
+    }
+    if(n == 1) return 0;
+    else return (p == 1);
+}
