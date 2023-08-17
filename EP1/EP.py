360de74b3d0bb10d584bf87d8281ca399386c370
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-08-16 21:09:48 -0300

    Passo 1 em Python do EP1 feito

--- EP1/EP.py
+++ EP1/EP.py
@@ -1,24 +1,44 @@
 TAM = 16
 
 def main():
+    # Lendo a entrada:
     string_entrada = input("Digite uma string: ")
-    tamanho_entrada = len(string_entrada)
 
-    num_blocos = int(tamanho_entrada / TAM)
-    resto = tamanho_entrada % TAM
+    # Parâmetros importantes:
+    tam_entrada = len(string_entrada)
+    num_blocos = int(tam_entrada / TAM)
 
-    if num_blocos * TAM < tamanho_entrada:
+    if num_blocos * TAM < tam_entrada:
         num_blocos = num_blocos + 1
-    new_pos = TAM * num_blocos - tamanho_entrada
+    new_pos = TAM * num_blocos - tam_entrada
+    # new_pos - Indica quantos espaços em um dos blocos serão preenchidos com alguma informação aleatória;
 
 
-    # num_blocos - Indica quantos blocos de 16 bytes serão necessários para guardar a string;
-    # resto - Indica quantos espaços em um dos blocos serão preenchidos com alguma informação aleatória;
+    """ ----- PASSO 1 - Ajuste do Tamanho: ----- """
+
+    # Colocando em uma lista o código em ASCII de cada caractere da entrada:
+    saidaPassoUm = []
+    for i in range(len(string_entrada)):
+        saidaPassoUm.append(ord(string_entrada[i]))
+    
+    # Preenchendo os espaços até ter um múltiplo de 16 com: 16 - tam_entrada % 16
+    print("Posições a serem preenchidas: ", new_pos, '\n')
+    for i in range(new_pos):
+        saidaPassoUm.append(TAM - tam_entrada % TAM)
+
+    """ 
+     for i in range(len(saidaPassoUm)):
+        print(i, ": ", saidaPassoUm[i])
+
+    print(saidaPassoUm) 
+    """ 
+
+    """ ----- PASSO 2 - Cálculo e Contanação dos XOR ----- """
+
+
 
     
-    print(num_blocos)
-    print(tamanho_entrada)
-    print(new_pos)
+    
 
 
 main()
