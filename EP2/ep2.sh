aa9fac363be5284bed5849ebb6095f91493da6d8
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-10-01 18:39:38 -0300

    Começando EP2

---
+++ EP2/ep2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# AUTOR:
+# Seu nome, NUSP e endereço de e-mail
+
+# DESCRIÇÃO:
+# Explique o que os programas fazem (Não diga apenas que ele é o EP2 da
+# disciplina tal, explique o que de fato os programas fazem)
+
+# COMO EXECUTAR:
+# Informe como executar o cliente e o servidor e quais os parâmetros
+# de linha de comando necessários
+
+# TESTES:
+# Forneça explicações sobre o testa.c
+
+# DEPENDÊNCIAS:
+# Informe o que é necessário para rodar o programa. Informações como o
+# SO onde você executou e sabe que ele funciona são importantes de serem
+# colocadas aqui.
