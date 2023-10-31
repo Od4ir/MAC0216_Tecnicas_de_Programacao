b565c634abe531c30f5d279d0567bdba7d7089f6
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-10-31 13:43:11 -0300

    test

--- EP2/ep22.sh
+++ EP2/ep22.sh
@@ -10,15 +10,3 @@ read -a op
 echo ${op[1]}
 
 OPCOES="op1 op2 op3"
-
-select opt in ${OPCOES}; do
-    if [ "$opt" == "op1" ]; then
-        echo "oi"
-    elif [ "$opt" == "op2" ]; then
-        echo "op2"
-    elif [ "$opt" == "op3" ]; then
-        echo "op3"
-    else 
-        echo "Inválido"
-    fi 
-done
