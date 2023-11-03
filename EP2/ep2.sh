eb16487c5a61be7f790561a7bfbfbd2286f8fbeb
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-11-03 20:21:08 -0300

    Continuação EP2

--- EP2/ep2.sh
+++ EP2/ep2.sh
@@ -215,6 +215,9 @@ elif [ $1 = "cliente" ]; then
         elif [ ${op} = "passwd" ]; then
             muda_senha_usuario ${op[1]} ${op[2]} ${op[3]}
 
+        elif [ ${op} = "logout" ]; then
+            muda_senha_usuario ${op[1]} ${op[2]} ${op[3]}
+
         elif [ ${op} = "user" ]; then
             lista_usuarios_existentes
 
