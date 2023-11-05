96c149b27ebcf5cdffe38a505f6cc64413f2e34d
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-11-05 17:49:25 -0300

    Avanços no EP2

--- EP2/ep2.sh
+++ EP2/ep2.sh
@@ -18,21 +18,9 @@
 # SO onde você executou e sabe que ele funciona são importantes de serem
 # colocadas aqui.
 
-# Variáveis gerais:
 CONTADOR=0
-MSG_TELEGRAM=""
-USER=""
-
-# Arquivos importantes:
 LOGADOS="/tmp/logados.txt"
-USERS="/tmp/usuarios_info.txt"
 USERS_INFO="/tmp/usuarios.txt"
-
-touch "${LOGADOS}"
-touch "${USERS}"
-touch "${USERS_INFO}"
-
-tempo_inicial=$(date +%s)
 
 # Recebe uma mensagem e envia para o Telegram;
 function envia_msg_telegram {
@@ -48,16 +36,15 @@ function mostrar_tempo {
 
 # Lista os usuários do arquivo 'logados.txt'
 function lista_usuarios_logados {
+    echo ${LOGADOS}
     cat ${LOGADOS}
 }
 
 # Apaga os arquivos de informações do servidor;
 function reset_servidor {
     rm -f ${LOGADOS} 
-    rm -f ${USERS}
     rm -f ${USERS_INFO}
     touch "${LOGADOS}"
-    touch "${USERS}"
     touch "${USERS_INFO}"
 }
 
@@ -95,105 +82,73 @@ function login_usuario {
         if [ "${result}" == $2 ]; then
             echo "$1" >> ${LOGADOS}
 
-            USER="/tmp/$1"
-            echo ${USER}
-            if [[ ! -p ${USER} ]]; then
-                mkfifo ${USER}
+            echo "/tmp/$1" > ${USER}
+            AUX=$(cat ${USER})
+            echo ${AUX}
+            if [[ ! -p ${AUX} ]]; then
+                mkfifo ${AUX}
             fi
-            trap "rm -f ${USER}" EXIT
+            trap "rm -f ${AUX}" EXIT
+
+            MSG_TELEGRAM="Usuárix $1 fez login"
+            envia_msg_telegram "${MSG_TELEGRAM}"
 
         else 
             echo "Senha incorreta"
-            MSG="Usuárix $1 errou a senha"
-            envia_msg_telegram ${MSG[*]}
+            MSG_TELEGRAM="Usuárix $1 errou a senha"
+            envia_msg_telegram "${MSG_TELEGRAM}"
         fi
     else 
         echo "Usuário não encontrado"
     fi
-    # CONTADOR=0
-    # while [ ${CONTADOR} -lt ${#USUARIOS[@]} ]; do
-    #     if [ ${USUARIOS[${CONTADOR}]} == $1 ]; then
-    #         if [ ${SENHAS[${CONTADOR}]} == $2 ]; then
-    #             if [ ${LOGADOS[$CONTADOR]} -eq 0 ]; then
-
-    #                 LOGADOS[${CONTADOR}]=1
-
-    #                 AUX=${USUARIOS[${CONTADOR}]}
-    #                 MSG_TELEGRAM="Usuárix ${AUX} fez login"
-    #                 envia_msg_telegram "${MSG_TELEGRAM}"
-    #                 echo ${AUX} >> "${LOGADOS}"
-
-    #                 USER="/tmp/${AUX}"
-    #                 if [[ ! -p ${USER} ]]; then
-    #                     mkfifo ${USER}
-    #                 fi
-    #                 trap "rm -f ${USER}" EXIT
-    #             else
-    #                 echo "Usuário já logado"
-    #             fi
-    #         else
-    #             echo "Senha incorreta"
-    #         fi
-    #     fi
-    #     let CONTADOR=CONTADOR+1
-    # done
 }
 
 function logout_usuario {
     MSG_TELEGRAM=()
     MSG_TELEGRAM="<LOGOUT REALIZADO> Usuário $1 fez logout"
-    envia_msg_telegram ${MSG_TELEGRAM[*]}
+    envia_msg_telegram "${MSG_TELEGRAM}"
     sed -i "/$1/d" "${LOGADOS}"
     rm -f ${USER}
-    USER=""
     # Faz logout do usuário do pipe atual;
 }
 
 function mensagem_usuario {
-    # Pega o usuário do pipe atual; ??
-    # Pega o destinatário; $1
-    # Pega a mensagem; $2
-    # Envia a mensagem para o usuário destinatário;
     aux=$1
     user=$2
-    shift 2
+    sender=$3
+    shift 3
     mensagem="$@"
     result=$(echo "${mensagem}" | sed "s/${aux} ${user}//g")
-    echo ${result}
     dest="/tmp/${user}"
-    echo ${dest}
-    echo "${result[*]}" >${dest}
+    msg="[Mensagem do ${sender}]: ${result}"
+    echo "${msg}" >${dest}
+    echo "cliente> " >${dest}
 }
 
-function apagar_arquivos {
-    rm -f ${LOGADOS} 
-    rm -f ${USERS}
+function limpa_arquivos {
+    rm -f ${LOGADOS}
     rm -f ${USERS_INFO}
 }
 
-while [ 1 ]; do
-    if [ -s "${LOGADOS}" ]; then
-        conteudo=$(cat "${LOGADOS}")
-        envia_msg_telegram ${conteudo}
-    fi
-    sleep 16s
-done &
-
-PRIMEIROPLANO=$!
-
-while [ 1 ]; do
-     if [ "${USER}" != "" ]; then
-        conteudo=$(cat <${USER})
-        echo ${CONTADOR} ${conteudo}
-        let CONTADOR=CONTADOR+1
-     fi
- done &
-
-SEGUNDOPLANO=$!
-
 if [ $1 = "servidor" ]; then
+    tempo_inicial=$(date +%s)
+
+    touch "${LOGADOS}"
+    touch "${USERS_INFO}"
+
     echo " >>> MODO SERVIDOR LIGADO <<< "
     # Execução principal do modo servidor:
+
+    while [ 1 ]; do
+        if [ -s "${LOGADOS}" ]; then
+            let CONTADOR=CONTADOR+1
+            conteudo=$(cat ${LOGADOS})
+            envia_msg_telegram "${CONTADOR}: ${conteudo}"
+        fi
+        sleep 16s
+    done &
+
+    LOOP_MSG_TELEGRAM=$!
 
     while [ 1 ]; do
         # Leitura do comando escolhido pelo usuário:
@@ -205,15 +160,17 @@ if [ $1 = "servidor" ]; then
             lista_usuarios_logados
 
         elif [ ${op} = "time" ]; then
-            mostrar_tempo
+            tempo_atual=$(date +%s)
+            tempo_passado=$((tempo_atual - tempo_inicial))
+            echo "${tempo_passado} segundos."
 
         elif [ ${op} = "reset" ]; then
             reset_servidor
             
         elif [ ${op} = "quit" ]; then
             echo "Saindo"
-            kill -15 ${PRIMEIROPLANO}
-            kill -15 ${SEGUNDOPLANO}
+            kill -15 ${LOOP_MSG_TELEGRAM}
+            limpa_arquivos
             exit 0
 
         else
@@ -222,6 +179,14 @@ if [ $1 = "servidor" ]; then
     done
 
 elif [ $1 = "cliente" ]; then
+
+    if [ ! -e "/tmp/usuarios.txt" ]; then
+        echo "Servidor não iniciado!"
+        exit 0
+    fi
+
+    USER=$(mktemp)
+
     echo -e " >>> SEJA BEM VINDX <<< \n"
      # Execução principal do modo cliente:
     echo " Lista de opções disponíveis: "
@@ -235,6 +200,19 @@ elif [ $1 = "cliente" ]; then
     echo "  >>> list.........................[ Lista usuários logados ]"
     echo "  >>> logout.......................[ Faz logout do sistema ]"
     echo -e "  >>> msg usuario mensagem.........[ Envia mensagem para outro usuário ]\n"
+
+    while [ 1 ]; do
+        if [ -s "${USER}" ]; then
+            AUX=$(cat ${USER})
+            if [ ! -s "$AUX" ]  && [ -e "$AUX" ]; then
+                conteudo=$(cat <${AUX})
+                echo "${conteudo}"
+            fi
+        fi
+    done &
+
+    LOOP_RECEBE_MSG=$!
+
     while [ 1 ]; do
         # Leitura do comando escolhido pelo usuário:
         echo -n "cliente> " 
@@ -243,26 +221,21 @@ elif [ $1 = "cliente" ]; then
         # Execução do comando escolhido pelo usuário:
         if [ ${op[0]} = "quit" ]; then
             echo "Saindo"
-            kill -15 ${PRIMEIROPLANO}
-            kill -15 ${SEGUNDOPLANO}
-            apagar_arquivos
+            kill -15 ${LOOP_RECEBE_MSG}
+            rm -f ${USER}
             exit 0
 
         elif [ ${op[0]} = "create" ]; then
             cria_usuario ${op[1]} ${op[2]}
 
         elif [ ${op} = "list" ]; then
-            echo "Listando usuários"
             lista_usuarios_logados
-
-        elif [ ${op} = "time" ]; then
-            mostrar_tempo
 
         elif [ ${op} = "magic" ]; then
             cat ${ARQ}
 
         elif [ ${op} = "login" ]; then
-            if [ "${USER}" == "" ]; then
+            if [ ! -s "${USER}" ]; then
                 login_usuario  ${op[1]} ${op[2]}
             else 
                 echo "Você já está logado!"
@@ -275,7 +248,9 @@ elif [ $1 = "cliente" ]; then
             logout_usuario ${op[1]}
 
         elif [ ${op} = "msg" ]; then
-            mensagem_usuario ${op[0]} ${op[1]} ${op[@]}
+            # AUX=$(cat ${USER})
+            remetente=$(cat "${USER}" | sed "s/\/tmp\///g")
+            mensagem_usuario ${op[0]} ${op[1]} ${remetente} ${op[@]}
 
         elif [ ${op} = "panda" ]; then
             cat ${USERS_INFO}
@@ -290,6 +265,7 @@ fi
 
 kill -15 ${PRIMEIROPLANO} 
 kill -15 ${SEGUNDOPLANO}
+limpa_arquivos
 exit 0
 
 
