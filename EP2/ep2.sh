c4765fe5ac5a6b0f986af8e85fc6ab3b827a23de
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-11-05 00:32:02 -0300

    Adição do sistema de arquivos + problema do loop de mensagens

--- EP2/ep2.sh
+++ EP2/ep2.sh
@@ -18,106 +18,134 @@
 # SO onde você executou e sabe que ele funciona são importantes de serem
 # colocadas aqui.
 
-# VARIÁVEIS:
-
+# Variáveis gerais:
 CONTADOR=0
-USUARIOS=()
-LOGADOS=()
-SENHAS=()
 MSG_TELEGRAM=""
 USER=""
-ARQ="/tmp/logados.txt"
+
+# Arquivos importantes:
+LOGADOS="/tmp/logados.txt"
+USERS="/tmp/usuarios_info.txt"
 USERS_INFO="/tmp/usuarios.txt"
 
-touch "${ARQ}"
+touch "${LOGADOS}"
+touch "${USERS}"
+touch "${USERS_INFO}"
+
 tempo_inicial=$(date +%s)
 
-trap "rm -f ${ARQ}" EXIT
-
+# Recebe uma mensagem e envia para o Telegram;
 function envia_msg_telegram {
     curl -s https://api.telegram.org/bot6599211463:AAGKSxJsGbU6kuqAvzJgxcKbDOrB6G2Uxag/sendMessage -d chat_id=1360171414 -d text="$1" 1>/dev/null
 }
 
+# Exibe o tempo em que o servidor está ligado;
 function mostrar_tempo {
     tempo_atual=$(date +%s)
     tempo_passado=$((tempo_atual - tempo_inicial))
     echo "${tempo_passado} segundos."
 }
 
+# Lista os usuários do arquivo 'logados.txt'
 function lista_usuarios_logados {
-    CONTADOR=0
-    if [ -s "${ARQ}" ]; then
-        echo -e "Usuários logados: \n"
-        conteudo=$(cat "${ARQ}")
-        echo ${conteudo}
+    cat ${LOGADOS}
+}
+
+# Apaga os arquivos de informações do servidor;
+function reset_servidor {
+    rm -f ${LOGADOS} 
+    rm -f ${USERS}
+    rm -f ${USERS_INFO}
+    touch "${LOGADOS}"
+    touch "${USERS}"
+    touch "${USERS_INFO}"
+}
+
+# Registra um usuário no arquivo usuarios.txt e usuarios_info.txt
+function cria_usuario {
+    if grep -q "$1;" ${USERS_INFO}; then
+        echo "Username indisponível" 
+    else
+        echo "$1;$2" >> ${USERS_INFO}
+        # Isso é feito para permitir que se crie um usuário
+        # que seja igual a senha de outro usuário;
     fi
 }
 
-function reset_servidor {
-    USUARIOS=()
-    SENHAS=()
-    LOGADOS=()
-}
-
-function cria_usuario {
-    for nome_usuario in ${USUARIOS[*]}; do
-        if [ ${nome_usuario} == $1 ]; then
-            echo "ERRO!"
-            return 1
-        fi
-    done
-    USUARIOS+=($1)
-    SENHAS+=($2)
-    LOGADOS+=(0)
-}
-
 function muda_senha_usuario {
-    CONTADOR=0
-    while [ ${CONTADOR} -lt ${#USUARIOS[*]} ]; do
-        if [ ${USUARIOS[${CONTADOR}]} == $1 ] && [ ${SENHAS[$CONTADOR]} == $2 ]; then
-            SENHAS[${CONTADOR}]=$3
-            return 0
+    info=$(grep "$1;" "${USERS_INFO}")
+    if [ -n "${info}" ]; then
+        senha=$(echo "${info}" | sed "s/$1;//g")
+        if [ "${senha}" == "$2" ]; then
+            sed -i "s/$1;$2/$1;$3/" ${USERS_INFO}
+        else 
+            echo "Senha incorreta"
+            echo "ERRO!"
         fi
-        let CONTADOR=CONTADOR+1
-    done
-    echo "ERRO!"
+    else 
+        echo "Usuário não encontrado"
+        echo "ERRO!"
+    fi
 }
 
 function login_usuario {
-    CONTADOR=0
-    while [ ${CONTADOR} -lt ${#USUARIOS[@]} ]; do
-        if [ ${USUARIOS[${CONTADOR}]} == $1 ]; then
-            if [ ${SENHAS[${CONTADOR}]} == $2 ]; then
-                if [ ${LOGADOS[$CONTADOR]} -eq 0 ]; then
+    info=$(grep "$1;" "${USERS_INFO}")
+    if [ -n "${info}" ]; then        
+        result=$(echo "${info}" | sed "s/$1;//g")
+        if [ "${result}" == $2 ]; then
+            echo "$1" >> ${LOGADOS}
 
-                    LOGADOS[${CONTADOR}]=1
-
-                    AUX=${USUARIOS[${CONTADOR}]}
-                    MSG_TELEGRAM="Usuárix ${AUX} fez login"
-                    envia_msg_telegram "${MSG_TELEGRAM}"
-                    echo ${AUX} >> "${ARQ}"
-
-                    USER="/tmp/${AUX}"
-                    if [[ ! -p ${USER} ]]; then
-                        mkfifo ${USER}
-                    fi
-                    trap "rm -f ${USER}" EXIT
-                else
-                    echo "Usuário já logado"
-                fi
-            else
-                echo "Senha incorreta"
+            USER="/tmp/$1"
+            echo ${USER}
+            if [[ ! -p ${USER} ]]; then
+                mkfifo ${USER}
             fi
+            trap "rm -f ${USER}" EXIT
+
+        else 
+            echo "Senha incorreta"
+            MSG="Usuárix $1 errou a senha"
+            envia_msg_telegram ${MSG[*]}
         fi
-        let CONTADOR=CONTADOR+1
-    done
+    else 
+        echo "Usuário não encontrado"
+    fi
+    # CONTADOR=0
+    # while [ ${CONTADOR} -lt ${#USUARIOS[@]} ]; do
+    #     if [ ${USUARIOS[${CONTADOR}]} == $1 ]; then
+    #         if [ ${SENHAS[${CONTADOR}]} == $2 ]; then
+    #             if [ ${LOGADOS[$CONTADOR]} -eq 0 ]; then
+
+    #                 LOGADOS[${CONTADOR}]=1
+
+    #                 AUX=${USUARIOS[${CONTADOR}]}
+    #                 MSG_TELEGRAM="Usuárix ${AUX} fez login"
+    #                 envia_msg_telegram "${MSG_TELEGRAM}"
+    #                 echo ${AUX} >> "${LOGADOS}"
+
+    #                 USER="/tmp/${AUX}"
+    #                 if [[ ! -p ${USER} ]]; then
+    #                     mkfifo ${USER}
+    #                 fi
+    #                 trap "rm -f ${USER}" EXIT
+    #             else
+    #                 echo "Usuário já logado"
+    #             fi
+    #         else
+    #             echo "Senha incorreta"
+    #         fi
+    #     fi
+    #     let CONTADOR=CONTADOR+1
+    # done
 }
 
 function logout_usuario {
     MSG_TELEGRAM=()
     MSG_TELEGRAM="<LOGOUT REALIZADO> Usuário $1 fez logout"
     envia_msg_telegram ${MSG_TELEGRAM[*]}
-    sed -i "/$1/d" "${ARQ}"
+    sed -i "/$1/d" "${LOGADOS}"
+    rm -f ${USER}
+    USER=""
     # Faz logout do usuário do pipe atual;
 }
 
@@ -126,13 +154,26 @@ function mensagem_usuario {
     # Pega o destinatário; $1
     # Pega a mensagem; $2
     # Envia a mensagem para o usuário destinatário;
-    echo "mensagem"
+    aux=$1
+    user=$2
+    shift 2
+    mensagem="$@"
+    result=$(echo "${mensagem}" | sed "s/${aux} ${user}//g")
+    echo ${result}
+    dest="/tmp/${user}"
+    echo ${dest}
+    echo "${result[*]}" >${dest}
+}
+
+function apagar_arquivos {
+    rm -f ${LOGADOS} 
+    rm -f ${USERS}
+    rm -f ${USERS_INFO}
 }
 
 while [ 1 ]; do
-    if [ -s "${ARQ}" ]; then
-        echo -e "Usuários logados: \n"
-        conteudo=$(cat "${ARQ}")
+    if [ -s "${LOGADOS}" ]; then
+        conteudo=$(cat "${LOGADOS}")
         envia_msg_telegram ${conteudo}
     fi
     sleep 16s
@@ -141,12 +182,12 @@ done &
 PRIMEIROPLANO=$!
 
 while [ 1 ]; do
-    if [ "${USER}" != "" ]; then
+     if [ "${USER}" != "" ]; then
         conteudo=$(cat <${USER})
         echo ${CONTADOR} ${conteudo}
         let CONTADOR=CONTADOR+1
-    fi
-done &
+     fi
+ done &
 
 SEGUNDOPLANO=$!
 
@@ -185,7 +226,7 @@ elif [ $1 = "cliente" ]; then
      # Execução principal do modo cliente:
     echo " Lista de opções disponíveis: "
     echo -e "\n [ Login NÃO necessário: ]"
-    echo "  >>> create usuario senha..........[ Criação de usuário ]"
+    echo "  >>> create usuario senha.........[ Criação de usuário ]"
     echo "  >>> passwd usuario antiga nova...[ Mudança de senha ]"
     echo "  >>> login usuario senha..........[ Login ]"
     echo -e "  >>> quit.........................[ Sair do chat ]\n"
@@ -204,6 +245,7 @@ elif [ $1 = "cliente" ]; then
             echo "Saindo"
             kill -15 ${PRIMEIROPLANO}
             kill -15 ${SEGUNDOPLANO}
+            apagar_arquivos
             exit 0
 
         elif [ ${op[0]} = "create" ]; then
@@ -233,7 +275,10 @@ elif [ $1 = "cliente" ]; then
             logout_usuario ${op[1]}
 
         elif [ ${op} = "msg" ]; then
-            mensagem_usuario ${op[1]}
+            mensagem_usuario ${op[0]} ${op[1]} ${op[@]}
+
+        elif [ ${op} = "panda" ]; then
+            cat ${USERS_INFO}
 
         else 
             echo "Não há essa opção."
@@ -243,11 +288,13 @@ else
     echo "Opção não encontrada."
 fi
 
-kill -15 ${PRIMEIROPLANO}
+kill -15 ${PRIMEIROPLANO} 
 kill -15 ${SEGUNDOPLANO}
 exit 0
 
 
 # Garantir que o modo cliente não seja ativado sem o servidor;
 # Apagar os usuários quando fizerem logout;
+# Corrigir o time para os clientes;
+# O reset tem que zerar o tempo também?
 
