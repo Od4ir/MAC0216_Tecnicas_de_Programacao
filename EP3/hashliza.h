6c09344e74dc82c14a65aa0dda14e1ccbf976b27
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-12-08 11:21:16 -0300

    Exclusão dos protótipos das funções de printar no arquivo hashliza.h

--- EP3/hashliza.h
+++ EP3/hashliza.h
@@ -1,6 +1,4 @@
 /* Funções principais: */
-void printa_int(char * aux, int tamanho);
-void printa_char(char * aux);
 char * ep1Passo1Preenche(char *);
 char * ep1Passo2XOR(char *, int *, int *); 
 char * ep1Passo3Comprime(char *, int *, int); 
