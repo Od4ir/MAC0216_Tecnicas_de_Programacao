d242e9d08af1bc1e33725a2738005b026258adbd
Od4ir <101140991+Od4ir@users.noreply.github.com>
2023-12-08 22:23:21 -0300

    Adição dos comentários doxygen

--- EP3/shannon.c
+++ EP3/shannon.c
@@ -4,10 +4,6 @@
 #include <math.h>
 #include "shannon.h"
 
-/* É para ser uma biblioteca estática. Para compilar e gerar o .so:
- * gcc -c -fPIC -o shannon.o shannon.c
- * gcc -o libshannon.so -shared shannon.o
- */ 
 long double ep3CalculaEntropiaShannon(char * stringEntrada, int base) {
     int tam = strlen(stringEntrada);
     int menor = 10000000, maior = -10000000;
--- EP3/shannon.h
+++ EP3/shannon.h
@@ -3,7 +3,7 @@
 
 /*!
  * \brief Função que calcula a entropia de Shannon de uma string de
- * acordo com o apresentado em ... (Colocar as referências)
+ * acordo com o apresentado em https://youtu.be/maJ0oG-JzGw;
  * \param stringEntrada: Uma string de qualquer tamanho
  * \param base: A base do logaritmo a ser usada no cálculo da entropia
  * \return O valor da entropia de Shannon
